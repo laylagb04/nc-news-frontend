595c3e4f3310a177c2a1d897646e4a4c0295fc43
Layla Broadhead <laylabroadhead@Laylas-MacBook-Pro.local>
2024-11-04 17:08:32 +0000

    added components

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,17 @@
+import AllArticlesDisplay from "./AllArticlesDisplay"
+
+
+
+
+function Homepage(){
+    
+
+return (
+    <>
+<h1> Home</h1>
+<AllArticlesDisplay/>
+</>
+)
+}
+
+export default Homepage
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import {Link} from 'react-router-dom'
+
+function NavBar(){
+return (
+    <div className='navbar'> 
+<nav>
+<div>
+    <Link to='/'>Homepage</Link>
+</div>
+
+</nav>
+
+
+
+
+
+    </div>
+)
+
+}
+
+export default NavBar
