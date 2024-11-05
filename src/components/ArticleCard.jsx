634e8a2c09043ed50e1b9b59f57fee69fa286447
Layla Broadhead <laylabroadhead@Laylas-MBP.northcoders.com>
2024-11-05 16:40:17 +0000

    added articlecard

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -2,20 +2,28 @@ import {useParams} from 'react-router-dom'
 import {useLocation, Link} from 'react-router-dom'
 import { getArticleById } from '../axios'
 import {useState, useEffect} from 'react'
+import CommentCard from './CommentCard'
 
 function ArticleCard() {
 
 const [article, setArticle] = useState({})
+
+
+
+
 let { article_id} = useParams()
 
-console.log(article_id)
+
 useEffect(() => {
+   
     getArticleById(article_id).then((articleData ) => {
         setArticle(articleData)
     })
-}, [])
+}, [article_id])
 
-console.log(article, 'state')
+
+
+
 
 
 
@@ -38,23 +46,26 @@ return (
 
         
         <p className='article-body'> {article.body}</p>
-
+      
         <div className='article-vote-id-parent'>
             <div className='left'> 
         <p className='article-vote-id-child'>Comment Count: {article.comment_count}</p>
+       
+        
         <p className='article-vote-id-child'>Votes: {article.votes}</p>
         </div>
 
 
-        <div className='right'>
-
-
-      <p  className='article-vote-id-child'> Article ID: {article.article_id}</p>
-      <p className='article-vote-id-child'> Created at: {article.created_at}</p>
       </div>
-      </div>
+      <CommentCard article={article} />
 
-      
+
+
+      <div className='right'>
+<p  className='small-info'> Article ID: {article.article_id}</p>
+<p className='small-info'> Created at: {article.created_at}</p>
+
+</div>
         
     </div>
     </>
