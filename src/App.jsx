21c677a5f3e745bb65beeb4af7f47f5bbfda9910
Layla Broadhead <laylabroadhead@Laylas-MBP.northcoders.com>
2024-11-05 12:06:18 +0000

    added route for article card

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header'
 import NavBar from './components/NavBar'
 import {Routes, Route} from 'react-router-dom'
 import Homepage from './components/Homepage'
-
+import ArticleCard from './components/ArticleCard'
 
 
 
@@ -16,7 +16,8 @@ function App() {
 <Header/>
 <NavBar/>
 <Routes>
-<Route path='/' element={<Homepage />} />
+<Route path='/' element={<Homepage />}/>
+<Route path='/articles/:article_id' element={<ArticleCard />}/>
 </Routes>
 
 
