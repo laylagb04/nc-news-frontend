b4a3ff9d198fb1b83c0fc2dfecd158a9d75c9d2a
Layla Broadhead <laylabroadhead@laylas-mbp.home>
2024-11-07 09:56:34 +0000

    added function to get users and post comment

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,30 @@ import NavBar from './components/NavBar'
 import {Routes, Route} from 'react-router-dom'
 import Homepage from './components/Homepage'
 import ArticleCard from './components/ArticleCard'
-
-
-
-
 import './App.css'
+import { UserContext } from './context/users'
+import UserCard from './components/UserCard'
+
+
 
 function App() {
+const [loggedInUser, setLoggedInUser] = useState({})
+const isLoggedIn = Object.keys(loggedInUser).length > 0
+
+
   return (
+    <UserContext.Provider value={{loggedInUser, setLoggedInUser, isLoggedIn}}>
 <>
 <Header/>
 <NavBar/>
 <Routes>
 <Route path='/' element={<Homepage />}/>
 <Route path='/articles/:article_id' element={<ArticleCard />}/>
+<Route path='/login' element={<UserCard/>}/>
 </Routes>
 
-
-
-
 </>
+</UserContext.Provider>
   )
 }
  
