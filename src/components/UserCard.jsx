6389b11ad5716b174fc1e63907b075b54a538225
Layla Broadhead <laylabroadhead@laylas-mbp.home>
2024-11-07 09:55:48 +0000

    updated user card to match database users

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,52 @@
+import { UserContext } from "../context/users";
+import { useContext, useState, useEffect
+ } from "react";
+import { getUsers } from "../axios";
+
+
+function UserCard(){
+    const [users, setUsers] =useState([])
+
+    const {setLoggedInUser, loggedInUser} = useContext(UserContext)
+
+
+const [selectedUser, setSelectedUser] = useState(null)
+
+
+function UserSelection(user){
+    setLoggedInUser(user)
+    setSelectedUser(user)
+}
+
+useEffect(() => {
+getUsers().then((usersFromApi) => {
+setUsers(usersFromApi)
+})
+}, [])
+
+
+return (
+    <>
+    <h3> Login</h3>
+    {loggedInUser ? (
+    <div>
+        <h3> Welcome {loggedInUser.name} </h3>
+    </div>
+    ) : (
+        <p> Select a user to login </p>
+    )
+    }
+    <div className='user-div'>
+    {users.map(user => (
+        <div onClick={() => setLoggedInUser(user)}>
+        <img className='avatar-img' src={user.avatar_url}></img>
+        <p>{user.name}</p>
+        </div>
+        
+))}
+  </div>
+</>
+)
+}
+
+export default UserCard
