da582a94e3c200a30d9381e6d4fcf4e059c6e74a
Layla Broadhead <laylabroadhead@Laylas-MBP.northcoders.com>
2024-11-12 08:58:20 +0000

    added more classnames

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,32 @@
 import {Link} from 'react-router-dom'
 
 function NavBar(){
-return (
-    <div className='navbar'> 
-<nav>
-<div>
-    <Link to='/' className='link'>Homepage</Link>
-    <Link to='/login' className='link'>Login</Link>
-    <Link to='/topics' className='link'>Topics</Link>
-</div>
-
-</nav>
-
-
-
-
-
+    return (
+       <div className='navbar-div'>
+    <nav>
+    <div className='navbar-div-inner'>
+       <div className='navbar-homepage'>
+       <Link to='/' className='link navbar'>Homepage</Link>
+       </div>
+    
+    
+       <div className='navbar-login'>
+       <Link to='/login' className='link navbar'>Login</Link>
+       </div>
+    
+    
+       <div className='navbar-topics'>
+       <Link to='/topics' className='link navbar'>Topics</Link>
+       </div>
     </div>
-)
-
-}
+    
+    
+    </nav>
+       </div>
+    )
+    
+    
+    }
+    
 
 export default NavBar
